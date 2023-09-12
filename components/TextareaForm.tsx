96174ee37c0b6a9401bf8503b77fac62c6a72eca
Umang Patel <umangpatel3105@gmail.com>
2023-09-12 20:13:03 +0530

    ui changes

--- components/TextareaForm.tsx
+++ components/TextareaForm.tsx
@@ -1,65 +1,268 @@
-"use client"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@radix-ui/react-tabs";
+import React from "react";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import * as z from "zod"
-import { Button } from "../@/components/ui/button"
-import { Textarea } from "../@/components/ui/textarea"
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "../@/components/ui/form"
-import { toast } from "../@/components/ui/use-toast"
+type Props = {};
 
-const FormSchema = z.object({
-  bio: z
-    .string()
-    .min(10, {
-      message: "Bio must be at least 10 characters.",
-    })
-    .max(160, {
-      message: "Bio must not be longer than 30 characters.",
-    }),
-})
-
-export function TextareaForm() {
-  const form = useForm<z.infer<typeof FormSchema>>({
-    resolver: zodResolver(FormSchema),
-  })
-
-  function onSubmit(data: z.infer<typeof FormSchema>) {
-    toast({
-      title: "You submitted the following values:",
-      description: (
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-        </pre>
-      ),
-    })
-  }
-
+const TextAreaForm = (props: Props) => {
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6 text-white ">
-        <FormField
-          control={form.control}
-          name="bio"
-          render={({ field }) => (
-            <FormItem className="text-white" >
-              <FormLabel className="text-white" >Bio</FormLabel>
-              <FormControl>
-                <Textarea
-                  placeholder="Tell us a little bit about yourself"
-                  className="resize-none text-white "
-                  {...field}
+    <section className="py-10 bg-black sm:py-16 lg:py-24 w-screen ">
+      <div className="max-w-6xl px-4 mx-auto sm:px-6 lg:px-8">
+        <div className="grid grid-cols-1 md:items-stretch md:grid-cols-2 gap-x-12 lg:gap-x-20 gap-y-10">
+          <div className="flex flex-col justify-between lg:py-5">
+            <div>
+              <h2 className="text-3xl font-bold leading-tight text-white sm:text-4xl lg:leading-tight lg:text-5xl">
+                It’s time to build something exciting!
+              </h2>
+              <p className="max-w-xl mx-auto mt-4 text-base leading-relaxed text-white">
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                amet sint. Velit officia consequat duis.
+              </p>
+              <img
+                className="relative z-10 max-w-xs mx-auto -mb-16 md:hidden"
+                src="https://cdn.rareblocks.xyz/collection/celebration/images/contact/4/curve-line-mobile.svg"
+                alt=""
+              />
+
+              <img
+                className="hidden w-full translate-x-24 translate-y-8 md:block"
+                src="https://cdn.rareblocks.xyz/collection/celebration/images/contact/4/curve-line.svg"
+                alt=""
+              />
+            </div>
+
+            <div className="hidden md:mt-auto md:block">
+              <div className="flex items-center">
+                <svg
+                  className="w-6 h-6 text-yellow-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+                <svg
+                  className="w-6 h-6 text-yellow-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+                <svg
+                  className="w-6 h-6 text-yellow-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+                <svg
+                  className="w-6 h-6 text-yellow-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+                <svg
+                  className="w-6 h-6 text-yellow-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+              </div>
+
+              <blockquote className="mt-6">
+                <p className="text-lg leading-relaxed text-white">
+                  You made it so simple. My new site is so much faster and
+                  easier to work with than my old site. I just choose the page,
+                  make the change and click save.
+                </p>
+              </blockquote>
+
+              <div className="flex items-center mt-8">
+                <img
+                  className="flex-shrink-0 object-cover w-10 h-10 rounded-full"
+                  src="https://cdn.rareblocks.xyz/collection/celebration/images/contact/4/avatar.jpg"
+                  alt=""
                 />
-              </FormControl>
-              <FormDescription>
-                You can <span>@mention</span> other users and organizations.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button className="bg-white px-6 py-4 rounded-lg text-justify " type="submit">Submit</Button>
-      </form>
-    </Form>
-  )
-}
+                <div className="ml-4">
+                  <p className="text-base font-semibold text-white">
+                    Jenny Wilson
+                  </p>
+                  <p className="mt-px text-sm text-gray-400">
+                    Product Designer
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className=" justify-center">
+            <div className="overflow-hidden bg-white rounded-md">
+              <div className="p-6 sm:p-10">
+                <h3 className="text-3xl font-semibold text-black">
+                  Get a free quote
+                </h3>
+                <p className="mt-4 text-base text-gray-600">
+                  Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                  amet sint.
+                </p>
+                <Tabs defaultValue="account" className="w-[400px] ">
+                  <TabsList className="grid w-full grid-cols-2 p-2 rounded-lg ">
+                    <TabsTrigger value="txt">Text Post</TabsTrigger>
+                    <TabsTrigger value="img">Image Post</TabsTrigger>
+                  </TabsList>
+                  <TabsContent value="txt">
+                    <>
+                      <div className="py-4 px-2" >
+                        <label className="text-base font-medium text-gray-900">
+                          {" "}
+                          Project brief{" "}
+                        </label>
+                        <div className="mt-2.5 relative">
+                          <textarea
+                            name=""
+                            id=""
+                            placeholder="Enter your project brief"
+                            className="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md resize-y focus:outline-none focus:ring-orange-500 focus:border-orange-500 caret-orange-500"
+                            rows="4"
+                          ></textarea>
+                        </div>
+                      </div>
+
+                      <div>
+                        <button
+                          type="submit"
+                          className="inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold text-white transition-all duration-200 bg-orange-500 border border-transparent rounded-md focus:outline-none hover:bg-orange-600 focus:bg-orange-600"
+                        >
+                          Get Free Quote
+                        </button>
+                      </div>
+                    </>
+                  </TabsContent>
+                  <TabsContent value="img">
+                    <>
+
+                    </>
+                  </TabsContent>
+                </Tabs>
+
+                <form action="#" method="POST" className="mt-4">
+                  <div className="space-y-6">
+                    {/* <div>
+                      <label
+                        className="text-base font-medium text-gray-900"
+                      >
+                        {" "}
+                        Your name{" "}
+                      </label>
+                      <div className="mt-2.5 relative">
+                        <input
+                          type="text"
+                          name=""
+                          id=""
+                          placeholder="Enter your full name"
+                          className="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:ring-orange-500 focus:border-orange-500 caret-orange-500"
+                        />
+                      </div>
+                    </div> */}
+
+                    {/* <div>
+                      <label
+                        className="text-base font-medium text-gray-900"
+                      >
+                        {" "}
+                        Email address{" "}
+                      </label>
+                      <div className="mt-2.5 relative">
+                        <input
+                          type="text"
+                          name=""
+                          id=""
+                          placeholder="Enter your full name"
+                          className="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:ring-orange-500 focus:border-orange-500 caret-orange-500"
+                        />
+                      </div>
+                    </div> */}
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+
+          <div className="md:hidden">
+            <div className="flex items-center">
+              <svg
+                className="w-6 h-6 text-yellow-400"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+              </svg>
+              <svg
+                className="w-6 h-6 text-yellow-400"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+              </svg>
+              <svg
+                className="w-6 h-6 text-yellow-400"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+              </svg>
+              <svg
+                className="w-6 h-6 text-yellow-400"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+              </svg>
+              <svg
+                className="w-6 h-6 text-yellow-400"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+              </svg>
+            </div>
+
+            <blockquote className="mt-6">
+              <p className="text-lg leading-relaxed text-white">
+                You made it so simple. My new site is so much faster and easier
+                to work with than my old site. I just choose the page, make the
+                change and click save.
+              </p>
+            </blockquote>
+
+            <div className="flex items-center mt-8">
+              <img
+                className="flex-shrink-0 object-cover w-10 h-10 rounded-full"
+                src="https://cdn.rareblocks.xyz/collection/celebration/images/contact/4/avatar.jpg"
+                alt=""
+              />
+              <div className="ml-4">
+                <p className="text-base font-semibold text-white">
+                  Jenny Wilson
+                </p>
+                <p className="mt-px text-sm text-gray-400">Product Designer</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default TextAreaForm;
--- pages/post.tsx
+++ pages/post.tsx
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useTabStore } from "../store/store";
 import ImagePost from "../components/ImagePost";
 import TextPost from "../components/TextPost";
 import { Textarea } from "../@/components/ui/textarea";
 import { Label } from "../@/components/ui/label";
-import { TextareaForm } from "../components/TextAreaForm";
+import { client, exploreProfiles } from "../api/api";
+import Link from "next/link";
+import Image from "next/image";
+import TextareaForm from "../components/TextareaForm";
+// import { TextareaForm } from "../components/TextAreaForm";
 
 type Props = {
   activeTab: boolean;
@@ -13,10 +17,33 @@ type Props = {
 const Post = (props: Props) => {
   // const [activeTab, setactiveTab] = useState(1);
   const { activeTab, setactiveTab } = useTabStore();
+  const [profiles, setProfiles] = useState<any>([]);
+  useEffect(() => {
+    fetchProfiles();
+  }, []);
+
+  async function fetchProfiles() {
+    try {
+      /* fetch profiles from Lens API */
+      let response = await client.query({ query: exploreProfiles });
+      /* return profiles with profile pics  */
+      let profileData = await Promise.all(
+        response.data.exploreProfiles.items.filter(async (profileInfo) => {
+          return profileInfo.picture.__typename === "MediaSet";
+        })
+      );
+
+      /* update the local state with the profiles array */
+      setProfiles(profileData);
+    } catch (err) {
+      console.log({ err });
+    }
+  }
+
   return (
     <>
-      <section className="flex flex-1 h-screen w-screen pt-32 lg:py-4 bg-black ">
-        <div className="mb-4 border-b border-gray-200 dark:border-gray-700">
+      <section className="flex flex-1 h-screen w-screen lg:bg-black overflow-hidden">
+        {/* <div className="mb-4 border-b border-gray-200 dark:border-gray-700">
           <ul
             className="flex flex-wrap -mb-px text-sm font-medium text-center"
             id="myTab"
@@ -26,50 +53,8 @@ const Post = (props: Props) => {
             <TextPost />
             <ImagePost />
           </ul>
-        </div>
-
-        {activeTab == "text" ? (
-          <>
-            <div className="flex mb-6 border-b justify-center items-center border-gray-200 dark:border-gray-700">
-              <TextareaForm/>
-            </div>
-          </>
-        ) : (
-          <>
-            <div className="flex items-center justify-center w-1/2">
-              <label
-                // for="dropzone-file"
-                className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-neutral-900 hover:bg-stone-800 dark:border-gray-800 dark:hover:border-gray-500 dark:hover:bg-zinc-900"
-              >
-                <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                  <svg
-                    className="w-8 h-8 mb-4 text-gray-900 dark:text-gray-400"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 20 16"
-                  >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                    />
-                  </svg>
-                  <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                    <span className="font-semibold">Click to upload</span> or
-                    drag and drop
-                  </p>
-                  <p className="text-xs text-gray-500 dark:text-gray-400">
-                    SVG, PNG, JPG or GIF (MAX. 800x400px)
-                  </p>
-                </div>
-                <input id="dropzone-file" type="file" className="hidden" />
-              </label>
-            </div>
-          </>
-        )}
+        </div> */}
+        <TextareaForm />
       </section>
     </>
   );
